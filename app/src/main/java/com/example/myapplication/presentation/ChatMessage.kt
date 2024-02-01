c0772045c06fea0b99ca24f07c2e81fe24eda278
LuckyHariharan <lharihar@uwaterloo.ca>
2024-02-01 13:25:43 -0500

    delete files

--- app/src/main/java/com/example/myapplication/presentation/ChatMessage.kt
+++
@@ -1,8 +0,0 @@
-//package com.example.myapplication.presentation
-//
-//// /app/src/main/java/com/yourpackage/model/ChatMessage.kt
-//data class ChatMessage(
-//    val role: String,
-//    val content: String,
-//    val timestamp: String
-//)
