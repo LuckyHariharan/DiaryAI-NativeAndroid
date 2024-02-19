2aed22c35c071268f5e6202cb53f8b158513ff87
LuckyHariharan <lharihar@uwaterloo.ca>
2024-02-19 22:54:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation("com.google.ai.client.generativeai:generativeai:0.1.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
